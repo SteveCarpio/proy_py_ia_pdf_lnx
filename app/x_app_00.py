ad56202000d174401d61cfe2b04021372cd43e44
SteveCarpio <stv.madrid@gmail.com>
2025-09-17 12:40:39 +0200

    modelos ok cerrado

--- app/x_app_00.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def mostrar():
-
-    st.subheader("Pestaña 0")
-    st.write("Este es el contenido de la pestaña 0")
-
-    # Obtener IP del cliente si está disponible
-
--- app/x_app_03.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def mostrar():
-    st.subheader("Pestaña 3")
-    st.write("Este es el contenido de la pestaña 3")
