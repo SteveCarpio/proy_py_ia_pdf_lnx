0539ef388754572de236e02de32ce0813d70881a
SteveCarpio <stv.madrid@gmail.com>
2025-10-13 14:30:08 +0200

    app9 casi terminado

--- tmp/app9.py
+++ tmp/app9.py
@@ -30,6 +30,8 @@ df_excel = df_excel.fillna('')
 # Crea una lista y variables de apoyo
 filas_bono1 = []
 filas_bono2 = []
+filas_bono3 = []
+
 bonoX1, bonoX2, isinX1, isinX2, taaX1, taaX2, taaX3, taaX4, taaX5, taaX6 = "", "", "", "", "", "", "", "", "",""
 bonoX = ""
 contBlancos = 0
@@ -63,6 +65,9 @@ for idx, fila in df_excel.iterrows():
             var_c2 = datetime.datetime.strptime(var_c, "%Y-%m-%d %H:%M:%S").strftime("%d/%m/%Y")
             filas_bono2.append([bonoX, var_c2, round(float(var_d),2), round(float(var_f),2), round(float(var_h),2), round(float(var_j),2), round(float(var_k),2), round(float(var_m),2)])
 
+    if var_d == "(*)":
+        filas_bono3.append([bonoX, var_f])
+
     # ------- Fin del Bucle -------
     if var_c == "":
         contBlancos = contBlancos + 1
@@ -78,12 +83,25 @@ df_bono1 = pd.DataFrame(filas_bono1, columns=['BONO', 'ISIN', 'TAA_1', 'TAA_2', 
 df_bono1_union = pd.merge(df_bono1, df_numBono, on='BONO')
 df_bono1_union['N0'] = df_bono1_union.index.map(lambda x: x + 1)
 
+
+
 ### TRATAMIENTO DATAFRAME: BONO2
 df_bono2 = pd.DataFrame(filas_bono2, columns=['BONO', 'FECHA', 'AP_1', 'IB_1', 'AP_2', 'IB_2', 'AP_3', 'IB_3'])
 
+### TRATAMIENTO DATAFRAME: BONO3 --> INTERES BRUTO
+df_bono3 = pd.DataFrame(filas_bono3, columns=['BONO', 'INT_BRUTO'])
+
+
+### UNION BONO1 y BONO3: Agregar a la tabla bono1 el campo Ineres Bruto
+df_bono3_union = pd.merge(df_bono3, df_bono1_union, on='BONO')
+
+
+
 ### UNION DATAFRAME BONO1 y BONO2
-df_union1 = pd.merge(df_bono1_union, df_bono2, on='BONO')
-df_union1 = df_union1.reindex(columns=['N0', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'TAA_1', 'AP_1', 'IB_1', 'TAA_2', 'AP_2', 'IB_2', 'TAA_3', 'AP_3', 'IB_3'])
+df_union1 = pd.merge(df_bono3_union, df_bono2, on='BONO')
+df_union1 = df_union1.reindex(columns=['N0', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'INT_BRUTO', 'TAA_1', 'AP_1', 'IB_1', 'TAA_2', 'AP_2', 'IB_2', 'TAA_3', 'AP_3', 'IB_3'])
+
+
 
 ############### RESULTADO2 ############################################
 ### AGRUPAR BONOS Y SUMAR TOTALES
@@ -95,21 +113,21 @@ df_bono2_totales.rename(columns={'AP_1': 'T_AP_1', 'AP_2': 'T_AP_2', 'AP_3': 'T_
 ### AGREGAR RESULTADO 2 AL DF PRINCIPAL
 df_principal1 = pd.merge(df_bono2_totales, df_union1, on='BONO')
 
+
+
 ### CREP COLUMNA N1
 df_principal1['N1'] = df_principal1.index.map(lambda x: x + 1)
 
 ### ORDENAR CAMPOS
-df_principal1 = df_principal1.reindex(columns=['N0', 'N1', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'TAA_1', 'AP_1', 'IB_1', 'T_AP_1', 'TAA_2', 'AP_2', 'IB_2', 'T_AP_2', 'TAA_3', 'AP_3', 'IB_3', 'T_AP_3'])
+df_principal1 = df_principal1.reindex(columns=['N0', 'N1', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'INT_BRUTO', 'TAA_1', 'AP_1', 'IB_1', 'T_AP_1', 'TAA_2', 'AP_2', 'IB_2', 'T_AP_2', 'TAA_3', 'AP_3', 'IB_3', 'T_AP_3'])
 
-print("\n--- RESULTADO 2 ---")
-print(df_principal1.head(5))
-
+#print("\n--- RESULTADO 2 ---")
 
 ############### RESULTADO3 ############################################
 
-print("\n--- RESULTADO 3 ---")
+#print("\n--- RESULTADO 3 ---")
 # Columnas fijas que no cambian
-cols_fijas = ['N0', 'N1', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS']
+cols_fijas = ['N0', 'N1', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'INT_BRUTO']
 
 # Detectamos automáticamente los grupos (_1, _2, _3, etc.)
 grupos = sorted({col.split('_')[-1] for col in df_principal1.columns if '_' in col and col.split('_')[-1].isdigit()}, key=int)
@@ -122,10 +140,13 @@ filas2 = []
 filas3 = []
 filasx = []
 cont = 0
+sw = 1
 
 # Recorremos cada fila del DF original
 for _, fila in df_principal1.iterrows():
+
     cont = cont + 1
+    
     filas1.append({
         'N0': fila['N0'],
         'N1': fila['N1'],
@@ -135,6 +156,7 @@ for _, fila in df_principal1.iterrows():
         'FECHA': fila['FECHA'],
         'ISIN': fila['ISIN'],
         'NUM_BONOS': fila['NUM_BONOS'],
+        'INT_BRUTO': fila['INT_BRUTO'],
         'TAA': fila[f'TAA_1'],
         'AP': fila[f'AP_1'],
         'IB': fila[f'IB_1'],
@@ -150,6 +172,7 @@ for _, fila in df_principal1.iterrows():
         'FECHA': fila['FECHA'],
         'ISIN': fila['ISIN'],
         'NUM_BONOS': fila['NUM_BONOS'],
+        'INT_BRUTO': fila['INT_BRUTO'],
         'TAA': fila[f'TAA_2'],
         'AP': fila[f'AP_2'],
         'IB': fila[f'IB_2'],
@@ -164,16 +187,18 @@ for _, fila in df_principal1.iterrows():
         'FECHA': fila['FECHA'],
         'ISIN': fila['ISIN'],
         'NUM_BONOS': fila['NUM_BONOS'],
+        'INT_BRUTO': fila['INT_BRUTO'],
         'TAA': fila[f'TAA_3'],
         'AP': fila[f'AP_3'],
         'IB': fila[f'IB_3'],
         'T_AP': fila[f'T_AP_3']
     })
-    
+
 filasx = filas1 + filas2 + filas3
     
 # Creamos el nuevo DataFrame
 df_principal2 = pd.DataFrame(filasx)
+
 
 # Ordenamos el dataframe por campo2 y luego por campo3
 df_principal3 = df_principal2.copy() # es necesario hacerlo en un copia previa
@@ -184,9 +209,61 @@ df_principal3 = df_principal3.reset_index(drop=True)
 df_principal3['N4'] = df_principal3.index + 1
 
 ### ORDENO COLUMNAS DE SALIDA
-df_principal3 = df_principal3.reindex(columns=['N0', 'N1', 'N2', 'N3', 'N4', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'TAA', 'AP', 'IB', 'T_AP'])
+df_principal3 = df_principal3.reindex(columns=['N0', 'N1', 'N2', 'N3', 'N4', 'BONO', 'FECHA', 'ISIN', 'NUM_BONOS', 'INT_BRUTO', 'TAA', 'AP', 'IB', 'T_AP'])
 
+### CREO LA COLUMNA N4 - INTBRUTO - Campos TOTALES:  TT1, TT2
+filas4 = []
+cont2 = 0
+sw = 1
+for _, fila4 in df_principal3.iterrows():
 
+    # Variables temporales
+    v_numBonos = int(fila4['NUM_BONOS'])
+    v_tIntBrut = float(fila4['INT_BRUTO'])
+    v_totAmoPr = float(fila4['T_AP'])
+    v_amoPrinc = float(fila4['AP'])
+    v_intBruto = float(fila4['IB'])
 
-print(df_principal3)
-df_principal3.to_excel('/home/robot/Python/proy_py_ia_pdf_lnx/tmp/a_R3.xlsx', sheet_name='hoja1', index=False)
+    # Reinicio el contador para cada sub-grupo de N2    
+    if sw == fila4['N2']:
+        cont2 = cont2 + 1
+    else:
+        sw = fila4['N2']
+        cont2 = 1
+
+    # Creo variables de salida TT1 y TT2        
+    if cont2 == 1:
+        TT2 = (v_intBruto + v_tIntBrut) * v_numBonos
+        TT1 = (v_totAmoPr - v_amoPrinc) * v_numBonos
+    else:
+        TT2 = v_intBruto * v_numBonos
+        TT1  =  TT1 - (v_amoPrinc * v_numBonos)
+
+    filas4.append({
+        'N0': fila4['N0'],
+        'N1': fila4['N1'],
+        'N2': fila4['N2'],
+        'N3': fila4['N3'],
+        'N4': cont2,
+        'BONO': fila4['BONO'],
+        'FECHA': fila4['FECHA'],
+        'ISIN': fila4['ISIN'],
+        'NUM_BONOS': fila4['NUM_BONOS'],
+        'INT_BRUTO': float(fila4['INT_BRUTO']) if cont2 == 1 else 0,
+        'TAA': fila4['TAA'],
+        'AP': fila4['AP'],
+        'IB': fila4['IB'],
+        'T_AP': fila4['T_AP'],
+        'TT1': TT1,
+        'TT2': TT2
+    })
+    sw = fila4['N2']
+# Creo el dataframe
+df_principal4 = pd.DataFrame(filas4)
+
+### ELIMINO CAMPOS NO NECESARIOS
+df_principal5 = df_principal4.drop(['N1', 'N3'], axis=1).copy()
+
+############### RESULTADO5 ############################################
+print(df_principal5.head(20))
+df_principal5.to_excel('/home/robot/Python/proy_py_ia_pdf_lnx/tmp/a_R3.xlsx', sheet_name='hoja1', index=False)
